3518ec3af8f14c9af0732d9d006d3b6f59dc40c1
Christopher Ivan <gunardi.ivan@gmail.com>
2020-11-20 16:38:23 +0700

    add decode

--- Encrypt/encrypt.py
+++ Encrypt/encrypt.py
@@ -1,4 +1,9 @@
-def handleUpperCase(num, shift):
+# Christopher Ivan Gunardi
+# 18119025
+# Program Enkripsi-Dekripsi
+
+
+def handleUpperCase(num, shift):  # untuk upper case
     num += shift
     if num < ord("A"):
         num += 26
@@ -7,7 +12,7 @@ def handleUpperCase(num, shift):
     return num
 
 
-def handleLowerCase(num, shift):
+def handleLowerCase(num, shift):  # untuk lower case
     num += shift
     if num < ord("a"):
         num += 26
@@ -16,23 +21,31 @@ def handleLowerCase(num, shift):
     return num
 
 
-def findShift(idx):
+def findShift(idx, shift1, shift2, shift3):  # untuk mencari shift
     if (idx % 3 == 1):
-        return 3
+        return shift2
     elif (idx % 3 == 2):
-        return -3
-    return 4
+        return shift3
+    return shift1
 
 
-def encrypt(message):
+def cipher(message, shift1, shift2, shift3):  # fungsi utama
     encrypted = ""
     for (idx, char) in enumerate(message, 1):
         if char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
-            shift = findShift(idx)
+            shift = findShift(idx, shift1, shift2, shift3)
             num = ord(char)
             temp = handleUpperCase(num, shift) if char.isupper(
             ) else handleLowerCase(num, shift)
             encrypted += chr(temp)
         else:
             encrypted += char
-    print(encrypted)
+    return encrypted
+
+
+def encode(message):  # fungsi encode
+    return cipher(message, 4, 3, -3)
+
+
+def decode(message):  # fungsi decode
+    return cipher(message, -4, -3, 3)
